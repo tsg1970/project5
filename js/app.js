9dbefba20ad81b0a16be82ef85dd6c8122eeb5d5
tsg1970 <tsgoff@att.net>
2015-07-26 16:54:02 -0500

    Markers dropped and sync with list
    Able to get markers to appear/disappear based on the search query

--- js/app.js
+++ js/app.js
@@ -1,76 +1,137 @@
-$(function() {
 var locations = [
-        {
-        name: "Windhill Pancake Parlor",
-        street: "Elm St",
-        },
-        {
-        name: "Twisted Burger",
-        street: "Elm St",
-        },
-        {
-        name: "Green Street Cafe",
-        street: "Elm St",
-        },
-        {
-        name: "Olive Garden",
-        street: "Richmond Rd",
-        },
-        {
-        name: "Brunch Cafe",
-        street: "Route 31",
-        },
-        {
-        name: "Happy Jacks",
-        street: "Elm St",
-        },
-        {
-        name: "Vickie's Place",
-        street: "Elm St",
-        },
-        {
-        name: "Tacos El Norte",
-        street: "Elm St",
-        },
-        {
-        name: "Applebee's",
-        street: "Elm St",
-        },
-        {
-        name: "Chili's",
-        street: "Elm St",
-        },
-        {
-        name: "The Village Squire",
-        street: "Elm St",
-        }
-    ];
+    {
+    name: "Windhill Pancake Parlor",
+    street: "West Elm St",
+    lat: 42.3443318,
+    lng: -88.2626109
+    },
+    {
+    name: "Twisted Burger",
+    street: "West Elm St",
+    lat: 42.3463353,
+    lng: -88.2816521
+    },
+    {
+    name: "Green Street Cafe",
+    street: "North Green St",
+    lat: 42.3448488,
+    lng: -88.2670283
+    },
+    {
+    name: "Olive Garden",
+    street: "3451 Shoppers Dr",
+    lat: 42.367184,
+    lng: -88.2670474
+    },
+    {
+    name: "Brunch Cafe",
+    street: "414 South Route 31",
+    lat: 42.3229107,
+    lng: -88.273147
+    },
+    {
+    name: "Happy Jack's Submarine",
+    street: "4911 West Elm St",
+    lat: 42.3487341,
+    lng: -88.2936913
+    },
+    {
+    name: "Vickie's Place",
+    street: "1211 N River Rd",
+    lat: 42.3450892,
+    lng: -88.2599154
+    },
+    {
+    name: "Tacos El Norte",
+    street: "4318 West Elm St",
+    lat: 42.3479943,
+    lng: -88.2812933
+    },
+    {
+    name: "Applebee's",
+    street: "1700 N Richmond Rd",
+    lat: 42.350895,
+    lng: -88.266807
+    },
+    {
+    name: "Chili's Grill & Bar",
+    street: "2409 N Richmond Rd",
+    lat: 42.3609832,
+    lng: -88.2681989
+    },
+    {
+    name: "The Village Squire",
+    street: "West Elm St",
+    lat: 42.3480982,
+    lng: -88.2866883
+    }
+];
 
-var viewModel = {
-    query: ko.observable(''),
+var Model = {
+    markers: ko.observableArray()
 };
 
-function initialize() {
-    var myLatlng = new google.maps.LatLng(42.3455736, -88.2689808);
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 15
-    }
+function viewModel() {
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
-}
+    var self = this;
+    var map, mapOptions, marker;
+    self.query = ko.observable('');
 
-google.maps.event.addDomListener(window, 'resize', initialize);
-google.maps.event.addDomListener(window, 'load', initialize);
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(42.3455736, -88.2689808);
+        mapOptions = {
+            center: myLatlng,
+            zoom: 13
+        };
 
-viewModel.locations = ko.computed(function() {
-    var search = this.query().toLowerCase();
-        return ko.utils.arrayFilter(locations, function(location) {
-            return location.name.toLowerCase().indexOf(search)>= 0;
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        for (var x = 0; x < locations.length; x++) {
+            var locPosition = new google.maps.LatLng(locations[x].lat, locations[x].lng);
+            var marker = new google.maps.Marker({
+                position: locPosition,
+                title: locations[x].name,
+                animation: google.maps.Animation.DROP,
+                map: map
+            });
+            google.maps.event.addListener(marker, 'click', toggleBounce);
+            marker.setMap(map);
+            Model.markers.push(marker);
+        }//end for locations.length loop
+    }//end initialize
+
+    function toggleBounce() {
+            console.log("hi");
+        if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }//wns togglewBounce function
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    self.filteredArray = ko.computed(function() {
+        var search = self.query().toLowerCase();
+        return ko.utils.arrayFilter(Model.markers(), function(marker) {
+             return marker.title.toLowerCase().indexOf(search) >= 0;
+            });
+    }, self);
+
+
+    self.missingLocations = ko.computed(function() {
+        var differences = ko.utils.compareArrays(Model.markers(), self.filteredArray());
+        ko.utils.arrayForEach(differences, function(marker) {
+            if (marker.status === 'deleted') {
+               marker.value.setMap(null);
+            } else {
+                marker.value.setMap(map);
+            }
         });
-    }, viewModel);
+    }, self);
+} //end viewmModel function
+
 ko.applyBindings(viewModel);
-});
 
 document.querySelector("#nav-toggle").addEventListener("click", function() {
     document.getElementById("list").classList.toggle("noList");
