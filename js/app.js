e38cfde5d00194dc566cfa3668a32cadb9869420
tsg1970 <tsgoff@att.net>
2015-07-30 07:34:21 -0500

    Marker infowindow functionality added
    Marker infowindow was added from Foursquare data.

--- js/app.js
+++ js/app.js
@@ -1,72 +1,3 @@
-var locations = [
-    {
-    name: "Windhill Pancake Parlor",
-    street: "West Elm St",
-    lat: 42.3443318,
-    lng: -88.2626109
-    },
-    {
-    name: "Twisted Burger",
-    street: "West Elm St",
-    lat: 42.3463353,
-    lng: -88.2816521
-    },
-    {
-    name: "Green Street Cafe",
-    street: "North Green St",
-    lat: 42.3448488,
-    lng: -88.2670283
-    },
-    {
-    name: "Olive Garden",
-    street: "3451 Shoppers Dr",
-    lat: 42.367184,
-    lng: -88.2670474
-    },
-    {
-    name: "Brunch Cafe",
-    street: "414 South Route 31",
-    lat: 42.3229107,
-    lng: -88.273147
-    },
-    {
-    name: "Happy Jack's Submarine",
-    street: "4911 West Elm St",
-    lat: 42.3487341,
-    lng: -88.2936913
-    },
-    {
-    name: "Vickie's Place",
-    street: "1211 N River Rd",
-    lat: 42.3450892,
-    lng: -88.2599154
-    },
-    {
-    name: "Tacos El Norte",
-    street: "4318 West Elm St",
-    lat: 42.3479943,
-    lng: -88.2812933
-    },
-    {
-    name: "Applebee's",
-    street: "1700 N Richmond Rd",
-    lat: 42.350895,
-    lng: -88.266807
-    },
-    {
-    name: "Chili's Grill & Bar",
-    street: "2409 N Richmond Rd",
-    lat: 42.3609832,
-    lng: -88.2681989
-    },
-    {
-    name: "The Village Squire",
-    street: "West Elm St",
-    lat: 42.3480982,
-    lng: -88.2866883
-    }
-];
-
 var Model = {
     markers: ko.observableArray()
 };
@@ -74,7 +5,8 @@ var Model = {
 function viewModel() {
 
     var self = this;
-    var map, mapOptions, marker;
+    var map, mapOptions;
+    var locations;
     self.query = ko.observable('');
 
     function initialize() {
@@ -85,29 +17,40 @@ function viewModel() {
         };
 
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var infoWindow = new google.maps.InfoWindow();
+        fsURL = "https://api.foursquare.com/v2/venues/explore?ll=42.3455736, -88.2689808&section=food&limit=15&client_id=LJOOHSBYBD4RW3ZOSTYKIE2W0QDGHEUFV5B2TTXCWDSBSKYI&client_secret=AFW5DRM0A3XHYRLNJGMM3C014RB5BCEEITM4NO3TEFVMXBTP&v=20150701"
+        $.getJSON(fsURL, function(data) {
+            locations = data.response.groups[0].items
 
-        for (var x = 0; x < locations.length; x++) {
-            var locPosition = new google.maps.LatLng(locations[x].lat, locations[x].lng);
-            var marker = new google.maps.Marker({
-                position: locPosition,
-                title: locations[x].name,
-                animation: google.maps.Animation.DROP,
-                map: map
-            });
-            google.maps.event.addListener(marker, 'click', toggleBounce);
-            marker.setMap(map);
-            Model.markers.push(marker);
-        }//end for locations.length loop
+            for (var x = 0; x < locations.length; x++) {
+                var locPosition = new google.maps.LatLng(locations[x].venue.location.lat, locations[x].venue.location.lng);
+                if( locations[x].venue.url){
+                    var locURL = "<a href=" + locations[x].venue.url + ">" + locations[x].venue.url + "</a>"
+                } 
+                else{
+                    var locURL = ""
+                };
+                var contentString = "<span class='title'><b>" + locations[x].venue.name + "</b></span><br>" + 
+                        locations[x].venue.location.address + "<br>"+
+                        locations[x].venue.location.city + ", " +locations[x].venue.location.state + " " + locations[x].venue.location.postalCode + "<br>" +
+                        locURL+ "</a>";
+                var marker = new google.maps.Marker({
+                    position: locPosition,
+                    title: locations[x].venue.name,
+                    map: map,
+                    content: contentString
+                });
+                marker.setMap(map);
+                Model.markers.push(marker);
+                var contentString = locations[x].venue.name;
+                google.maps.event.addListener(marker, 'click', function(){
+                        infoWindow.setContent(this.content);
+                        infoWindow.open(map, this);
+                });
+            }//end for locations.length loop
+        })//end json
+   
     }//end initialize
-
-    function toggleBounce() {
-            console.log("hi");
-        if (marker.getAnimation() != null) {
-            marker.setAnimation(null);
-        } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }//wns togglewBounce function
 
     google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -117,7 +60,6 @@ function viewModel() {
              return marker.title.toLowerCase().indexOf(search) >= 0;
             });
     }, self);
-
 
     self.missingLocations = ko.computed(function() {
         var differences = ko.utils.compareArrays(Model.markers(), self.filteredArray());
