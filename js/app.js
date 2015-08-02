04f625a36b8720193e4bbad1dd4c9adea333ee8a
tsg1970 <tsgoff@att.net>
2015-08-02 11:18:01 -0500

    Code cleanup and add reference document
    Cleaned up the code and added the reference document

--- js/app.js
+++ js/app.js
@@ -15,25 +15,31 @@ function viewModel() {
             center: myLatlng,
             zoom: 13
         };
-
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         var infoWindow = new google.maps.InfoWindow();
+
+        // Using Foursquare API to retrieve list of restaurants
         fsURL = "https://api.foursquare.com/v2/venues/explore?ll=42.3455736, -88.2689808&section=food&limit=15&client_id=LJOOHSBYBD4RW3ZOSTYKIE2W0QDGHEUFV5B2TTXCWDSBSKYI&client_secret=AFW5DRM0A3XHYRLNJGMM3C014RB5BCEEITM4NO3TEFVMXBTP&v=20150701"
+        
         $.getJSON(fsURL, function(data) {
             locations = data.response.groups[0].items
 
+            // Loop through Foursquare results and add markers to the map. Create an infowindow for each marker.
             for (var x = 0; x < locations.length; x++) {
                 var locPosition = new google.maps.LatLng(locations[x].venue.location.lat, locations[x].venue.location.lng);
+                //if  statement to create a blank entry if there is no URL for the restaurant listing
                 if( locations[x].venue.url){
-                    var locURL = "<a href=" + locations[x].venue.url + ">" + locations[x].venue.url + "</a>"
+                    var locURL = '<a href=' + locations[x].venue.url + '>' + locations[x].venue.url + '</a>'
                 } 
                 else{
                     var locURL = ""
-                };
+                } //end if else statement
+                
+                // ContentString is for the infoWindow
                 var contentString = "<span class='title'><b>" + locations[x].venue.name + "</b></span><br>" + 
-                        locations[x].venue.location.address + "<br>"+
-                        locations[x].venue.location.city + ", " +locations[x].venue.location.state + " " + locations[x].venue.location.postalCode + "<br>" +
-                        locURL+ "</a>";
+                    locations[x].venue.location.address + "<br>"+
+                    locations[x].venue.location.city + ", " +locations[x].venue.location.state + " " + locations[x].venue.location.postalCode + "<br>" +
+                    locURL+ "</a><p>Data by Foursquare</p>";
                 var marker = new google.maps.Marker({
                     position: locPosition,
                     title: locations[x].venue.name,
@@ -44,16 +50,23 @@ function viewModel() {
                 Model.markers.push(marker);
                 var contentString = locations[x].venue.name;
                 google.maps.event.addListener(marker, 'click', function(){
-                        infoWindow.setContent(this.content);
-                        infoWindow.open(map, this);
+                    infoWindow.setContent(this.content);
+                    infoWindow.open(map, this);
                 });
             }//end for locations.length loop
         })//end json
-   
     }//end initialize
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-
+    //If statement to show blank page if Google Map is unavailable
+    if (typeof google === 'object' && typeof google.maps === 'object') {
+        google.maps.event.addDomListener(window, 'load', initialize);
+    } else {
+        document.getElementById('error-message').innerHTML = "Google Maps could not be loaded";
+        var myElement = document.querySelector('#error-message');
+        myElement.style.backgroundColor = "#eee";
+        $('#error-message').css('display','block'); //block error-message
+    };
+    //Filter list based on search entry
     self.filteredArray = ko.computed(function() {
         var search = self.query().toLowerCase();
         return ko.utils.arrayFilter(Model.markers(), function(marker) {
@@ -61,6 +74,7 @@ function viewModel() {
             });
     }, self);
 
+    // add or remove markers by comaring to list
     self.missingLocations = ko.computed(function() {
         var differences = ko.utils.compareArrays(Model.markers(), self.filteredArray());
         ko.utils.arrayForEach(differences, function(marker) {
@@ -75,6 +89,7 @@ function viewModel() {
 
 ko.applyBindings(viewModel);
 
+//show or unshow list when hamburger is clicked
 document.querySelector("#nav-toggle").addEventListener("click", function() {
     document.getElementById("list").classList.toggle("noList");
-})
+});
